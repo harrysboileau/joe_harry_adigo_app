8ae66a0f8ae8abdd010be3a30a4b2658478be390
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-29 14:18:01 -0500

    arguments specified

--- tdd.rb
+++ tdd.rb
@@ -1,5 +1,5 @@
 class Task
   def initialize(arg1, arg2)
-    
+
   end
 end
--- tdd_spec.rb
+++ tdd_spec.rb
@@ -1,10 +1,23 @@
-require 'spec_helper'
+require 'rspec'
 require_relative 'tdd'
 
+
+
 describe Task do
+  let(:title) { "Test title" }
+  let(:description) { "Test" }
+  let(:task) { Task.new(title, description) }
+
   describe "initialize" do
     it "should have 2 arguments" do
       expect { Task.new}.to raise_error(ArgumentError)
     end
+
+    it "title should be set" do
+      task.title.should eq.(title)
+    end
   end
 end
+
+
+
