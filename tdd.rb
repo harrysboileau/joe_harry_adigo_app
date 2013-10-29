8f6acb681ec7f640be8eaa010d880adfdc5d6fc2
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-29 15:04:23 -0500

    awesomeness!

--- tdd.rb
+++ tdd.rb
@@ -1,8 +1,16 @@
 class Task
-  attr_reader :title, :description
+  attr_reader :title, :description, :status, :created_at
   def initialize(title, description)
     @title = title
     @description = description
+    @complete = false
+    @created_at = Time.now
+  end
+
+  def status
+    unless @complete
+      return :incomplete
+    end
   end
 end
 
